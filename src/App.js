392c5767731ba7764ed2e295230150851d7f47d9
mariata06 <slinkywizard@mail.ru>
2023-02-24 20:22:56 +0300

    Remove warning message

--- src/App.js
+++ src/App.js
@@ -12,10 +12,12 @@ function App() {
 
   useEffect(() => {
     dispatch(calculateTotals());
+    // eslint-disable-next-line
   }, [cartItems]);
 
   useEffect(() => {
     dispatch(getCartItems('random'));
+    // eslint-disable-next-line
   }, []);
 
   if(isLoading) {
